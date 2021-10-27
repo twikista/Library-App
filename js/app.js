94724c2f618e5c3a9d0440e2f5862b8df84aa635
viro <twikista@gmail.com>
2021-10-27 21:50:11 +0100

    added constructor function

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//define array that store books
+const myLibrary = [];
+
+//Book constructor
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
