399e2b4844c5e81ea666c115026a2a801a7b7130
viro <twikista@gmail.com>
2021-10-27 21:58:15 +0100

    added functionality to add to book library and display books

--- js/app.js
+++ js/app.js
@@ -8,3 +8,9 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+
+//function that instantiate constructor and add book to list
+function addBookToList(title, author, pages, readStatus) {
+  const book = new Book(title, author, pages, readStatus);
+  myLibrary.push(book);
+}
