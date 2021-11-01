d3e1ad98877d10653c96e24606e6ac3518c76e33
viro <twikista@gmail.com>
2021-11-01 23:00:32 +0100

    added functionality to pop up edit modal

--- js/app.js
+++ js/app.js
@@ -18,6 +18,12 @@ Book.prototype.bookReadStatus = function (target) {
   target.textContent = `${readStatus}`;
 };
 
+Book.prototype.bookDetails = function (bookTitle, bookAuthor, bookPages) {
+  bookTitle.value = this.title;
+  bookAuthor.value = this.author;
+  bookPages.value = this.pages;
+};
+
 //get all books from local storage
 function getBookFromstorage() {
   return JSON.parse(localStorage.getItem("books")) || [];
@@ -35,6 +41,11 @@ myLibrary.forEach((book) => {
     }
     const readStatus = this.read === "true" ? "read" : "Not read";
     target.textContent = `${readStatus}`;
+  };
+  book.bookDetails = function (bookTitle, bookAuthor, bookPages) {
+    bookTitle.value = this.title;
+    bookAuthor.value = this.author;
+    bookPages.value = this.pages;
   };
 });
 
@@ -69,6 +80,7 @@ function displayBooks() {
       <td>${book.author}</td>
       <td>${book.pages}</td>
       <td><button class="btn read-btn" data-readstatusindex = "${index}">${readStatus}</button></td>
+      <td><button class="btn edit-book-btn" data-editbookindex = "${index}">Edit</button></td>
       <td><button class="btn delete-btn" data-bookindex = "${index}"><span class="material-icons delete-icon">delete</span></button></td>
       `;
 
@@ -79,14 +91,16 @@ function displayBooks() {
 function grabFormFieldVAlues() {
   const form = document.querySelector("#form");
   form.addEventListener("submit", (e) => {
+    const target = e.target;
+    console.log(target);
     e.preventDefault();
     //grab HTML form fields
-    const titleFied = form.elements["book-title"];
+    const titleField = form.elements["book-title"];
     const authorField = form.elements["book-author"];
     const pagesField = form.elements["book-pages"];
     const readStatusRadios = form.elements["read-status"];
     //grab the value of HTML form fields
-    const bookTitle = titleFied.value;
+    const bookTitle = titleField.value;
     const bookAuthor = authorField.value;
     const bookPages = pagesField.value;
     //get the value of selected radio button
@@ -100,15 +114,20 @@ function grabFormFieldVAlues() {
       return selectedValue;
     }
     const readStatus = getReadStatus();
-    //instantiate Book constructor
-    addBookToList(bookTitle, bookAuthor, bookPages, readStatus);
+    if (target.classList.contains("add-book-btn")) {
+      //instantiate Book constructor
+      console.log("hey");
+      addBookToList(bookTitle, bookAuthor, bookPages, readStatus);
+    } else if (target.classList.contains("save-btn")) {
+    }
+
     //clear input fields after adding each book
-    resetInputFields(titleFied, authorField, pagesField);
+    resetInputFields(titleField, authorField, pagesField);
   });
 }
 
-function resetInputFields(titleFied, authorField, pagesField) {
-  titleFied.value = "";
+function resetInputFields(titleField, authorField, pagesField) {
+  titleField.value = "";
   authorField.value = "";
   pagesField.value = "";
 }
@@ -186,17 +205,53 @@ function sortBooks() {
 
 sortBooks();
 
+function switchFormSubmitButton(target) {
+  const saveBtn = document.querySelector(".save-btn");
+  const addBtn = saveBtn.previousElementSibling;
+  if (target.classList.contains("edit-book-btn")) {
+    addBtn.classList.add("hide");
+    saveBtn.classList.add("active");
+  } else if (target.classList.contains("add-book-btn")) {
+    addBtn.classList.remove("hide");
+    saveBtn.classList.remove("active");
+  }
+}
 function openAddBookModal() {
   const addBookModalBtn = document.querySelector(".add-book-btn");
+  const titleField = form.elements["book-title"];
+  const authorField = form.elements["book-author"];
+  const pagesField = form.elements["book-pages"];
   addBookModalBtn.addEventListener("click", (e) => {
+    resetInputFields(titleField, authorField, pagesField);
     const target = e.target;
     const addBookModal = document.querySelector(".input-book-detail-modal");
-
+    switchFormSubmitButton(target);
     addBookModal.classList.add("active");
   });
 }
 
 openAddBookModal();
+
+function openEditBookModal() {
+  const tableBody = document.querySelector(".table-body");
+  function getBookDetails(instanceofBook) {
+    const titleField = form.elements["book-title"];
+    const authorField = form.elements["book-author"];
+    const pagesField = form.elements["book-pages"];
+    instanceofBook.bookDetails(titleField, authorField, pagesField);
+  }
+  tableBody.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.classList.contains("edit-book-btn")) {
+      getBookDetails(myLibrary[target.dataset.editbookindex]);
+      const addBookModal = document.querySelector(".input-book-detail-modal");
+      switchFormSubmitButton(target);
+      addBookModal.classList.add("active");
+    }
+  });
+}
+
+openEditBookModal();
 
 function closeAddBookModal() {
   const addBookModal = document.querySelector(".input-book-detail-modal");
@@ -210,3 +265,5 @@ function closeAddBookModal() {
 }
 
 closeAddBookModal();
+
+function editBook() {}
